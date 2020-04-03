e53a2e608e0111040d5b0345db0262080ab4e6a0
JureBevc <bevc.jure@gmail.com>
2020-04-03 20:47:37 +0200

    Database statistics.

---
+++ db/db_stats.py
@@ -0,0 +1,125 @@
+from db.database import DB
+
+db = DB()
+
+q = "SELECT l.from_page, COUNT(l.to_page) \
+ FROM crawldb.link as l \
+ GROUP BY l.from_page \
+ ORDER BY l.from_page"
+
+executed = db.execute(query=q, values=None)
+if executed:
+    db.conn.commit()
+    res = db.cur.fetchall()
+    if res:
+        avg = 0
+        for r in res:
+            avg += r[1]
+        avg = avg / len(res)
+        print(f"Average number of links per page: {avg}")
+
+
+q = "SELECT i.page_id, COUNT(i.id) \
+ FROM crawldb.image as i \
+ GROUP BY i.page_id \
+ ORDER BY i.page_id"
+
+executed = db.execute(query=q, values=None)
+if executed:
+    db.conn.commit()
+    res = db.cur.fetchall()
+    if res:
+        avg = 0
+        for r in res:
+            avg += r[1]
+        avg = avg / len(res)
+        print(f"Average number of images per page: {avg}")
+
+q = "SELECT COUNT(i.id) \
+ FROM crawldb.image as i"
+
+executed = db.execute(query=q, values=None)
+if executed:
+    db.conn.commit()
+    res = db.cur.fetchall()
+    if res:
+        print(f"Total number of images: {res[0][0]}")
+
+all_pages = 0
+
+q = "SELECT COUNT(id) \
+ FROM crawldb.page as i"
+
+executed = db.execute(query=q, values=None)
+if executed:
+    db.conn.commit()
+    res = db.cur.fetchall()
+    if res:
+        all_pages = res[0][0]
+        print(f"Total number of pages: {all_pages}")
+
+
+q = "SELECT COUNT(id) \
+ FROM crawldb.page as i \
+ WHERE i.page_type_code = 'HTML'"
+
+executed = db.execute(query=q, values=None)
+if executed:
+    db.conn.commit()
+    res = db.cur.fetchall()
+    if res:
+        print(f"Total number of pages of type HTML: {res[0][0]} ({res[0][0]*100/all_pages:.2f}%)")
+
+q = "SELECT COUNT(id) \
+ FROM crawldb.page as i \
+ WHERE i.page_type_code = 'DUPLICATE'"
+
+executed = db.execute(query=q, values=None)
+if executed:
+    db.conn.commit()
+    res = db.cur.fetchall()
+    if res:
+        print(f"Total number of pages of type DUPLICATE: {res[0][0]} ({res[0][0]*100/all_pages:.2f}%)")
+
+q = "SELECT COUNT(id) \
+ FROM crawldb.page as i \
+ WHERE i.page_type_code = 'FRONTIER'"
+
+executed = db.execute(query=q, values=None)
+if executed:
+    db.conn.commit()
+    res = db.cur.fetchall()
+    if res:
+        print(f"Total number of pages of type FRONTIER: {res[0][0]} ({res[0][0]*100/all_pages:.2f}%)")
+
+q = "SELECT url, COUNT(l.to_page) \
+FROM crawldb.link as l \
+INNER JOIN crawldb.page ON (l.from_page = crawldb.page.id) \
+GROUP BY url \
+ORDER BY COUNT(l.to_page) DESC"
+
+executed = db.execute(query=q, values=None)
+if executed:
+    db.conn.commit()
+    res = db.cur.fetchall()
+    if res:
+        print("Top 5 pages that contain most links to other pages: ")
+        for r in range(5):
+            print(f"  {r+1}: {res[r][0]} - {res[r][1]}")
+
+q = "SELECT url, COUNT(l.from_page) \
+FROM crawldb.link as l \
+INNER JOIN crawldb.page ON (l.to_page = crawldb.page.id) \
+GROUP BY url \
+ORDER BY COUNT(l.from_page) DESC"
+
+executed = db.execute(query=q, values=None)
+if executed:
+    db.conn.commit()
+    res = db.cur.fetchall()
+    if res:
+        print("Top 5 pages that contain most links from other pages: ")
+        for r in range(5):
+            print(f"  {r+1}: {res[r][0]} - {res[r][1]}")
+
+db.close()
