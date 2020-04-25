07088ef0c72db8e77724d479f6ff2be086348044
Luka Tavčer <lukatavcer@yahoo.com>
2020-04-25 23:56:37 +0200

    XPath

--- implementation-extraction/regex_extraction.py
+++
@@ -1,3 +0,0 @@
-def regex(page):
-    print("Start regex extraction")
-    pass
--- implementation-extraction/run-extraction.py
+++ implementation-extraction/run-extraction.py
@@ -1,14 +1,46 @@
 import sys
+import json
+from lxml import html
 
-regex = __import__("implementation-extraction.regex_extraction").regex_extraction.regex
-xpath = __import__("implementation-extraction.xpath_extraction").xpath_extraction.xpath
 
 pages = {
-    "input-extraction/rtvslo.si/Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html": "RTV",
-    "input-extraction/rtvslo.si/Volvo XC 40 D4 AWD momentum_ suvereno med najboljsÌe v razredu - RTVSLO.si.html": "RTV",
-    "input-extraction/overstock.com/jewelry01.html": "overstock",
-    "input-extraction/overstock.com/jewelry02.html": "overstock",
+    # "../input-extraction/rtvslo.si/Audi A6 50 TDI quattro_ nemir v premijskem razredu - RTVSLO.si.html": "rtv",
+    # "../input-extraction/rtvslo.si/Volvo XC 40 D4 AWD momentum_ suvereno med najboljsÌe v razredu - RTVSLO.si.html": "rtv",
+    "../input-extraction/overstock.com/jewelry01.html": "overstock",
+    "../input-extraction/overstock.com/jewelry02.html": "overstock",
 }
+
+
+def regex(page, site):
+    print("Start regex extraction")
+    pass
+
+
+def xpath(page, site):
+    print("Start XPath extraction")
+    tree = html.fromstring(page)
+    data = dict()
+
+    if site == "rtv":
+        data = dict(
+            Author=tree.xpath('//*[@id="main-container"]//*[@class="article-meta"]//div[@class="author-name"]/text()')[0].strip(),
+            PublishedTime=tree.xpath('//*[@id="main-container"]//*[@class="publish-meta"]/text()')[0].strip(),
+            Title=tree.xpath('//*[@id="main-container"]//header/h1/text()')[0].strip(),
+            SubTitle=tree.xpath('//*[@id="main-container"]//header/div[@class="subtitle"]/text()')[0].strip(),
+            Lead=tree.xpath('//*[@id="main-container"]//header/p[@class="lead"]/text()')[0].strip(),
+            Content="".join([p + "\n" for p in tree.xpath('//*[@id="main-container"]//div[@class="article-body"]//*[not(self::script)]/text()[normalize-space()]')])
+        )
+    elif site == "overstock":
+        data = dict(
+            Title='',
+            ListPrice='',
+            Price='',
+            Saving='',
+            SavingPercent='',
+            Content=''
+        )
+
+    return json.dumps(data)
 
 
 def main():
@@ -26,7 +58,7 @@ def main():
     data = []
 
     for page, site in pages.items():
-        page_content = open(page, 'r').read()
+        page_content = open(page, 'rb').read()
         data.append(algorithm(page_content, site))
 
     print("Extraction done")
