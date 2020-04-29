430235b05b5f72b182cb8dc28de6e2a418a2339f
JureBevc <bevc.jure@gmail.com>
2020-04-29 15:26:07 +0200

    Improved extraction with regex

--- implementation-extraction/run-extraction.py
+++ implementation-extraction/run-extraction.py
@@ -40,9 +40,14 @@ def regex(page, site):
     elif site == "overstock":
         data = []
         titles = re.findall("PROD_ID[^>]*><b>([^<]*)</b></a><br>", page)
-        list_price = re.findall("<b>List Price:</b></td><td align=\"left\" nowrap=\"nowrap\"><s>([^<]*)</s></td></tr>", page)
-        price = re.findall("<b>Price:</b></td><td align=\"left\" nowrap=\"nowrap\"><span class=\"bigred\"><b>([^<]*)</b></span></td></tr>", page)
-        saving = re.findall("<b>You Save:</b></td><td align=\"left\" nowrap=\"nowrap\"><span class=\"littleorange\">([^<]*)</span></td></tr>", page)
+        list_price = re.findall("<b>List Price:</b></td><td align=\"left\" nowrap=\"nowrap\"><s>([^<]*)</s></td></tr>",
+                                page)
+        price = re.findall(
+            "<b>Price:</b></td><td align=\"left\" nowrap=\"nowrap\"><span class=\"bigred\"><b>([^<]*)</b></span></td></tr>",
+            page)
+        saving = re.findall(
+            "<b>You Save:</b></td><td align=\"left\" nowrap=\"nowrap\"><span class=\"littleorange\">([^<]*)</span></td></tr>",
+            page)
         content = re.findall("</td><td valign=\"top\"><span class=\"normal\">([^<]*)<br>", page)
         for i in range(len(titles)):
             data.append(dict(
@@ -55,27 +60,21 @@ def regex(page, site):
             ))
     elif site == "bolha":
         data = []
-        titles = re.findall("<h3 class=\"entity-title\"><a[^>]*>([^<]*)</a></h3>", page)
+        titles = re.findall("<h3 class=\"entity-title\"><a[^>]*>([^<]*)</a></h3>(?=.*Zadnji oglasi)", page,
+                            re.MULTILINE | re.DOTALL)
         desc = re.findall("<div class=\"entity-description-main\">\n*\s*([^<]*)<br>", page)
-        price = re.findall("<strong class=\"price price--hrk\">\n*\s*([^<]*)&nbsp;<span class=\"currency\">([^<]*)</span>", page)
+        price = re.findall("<strong class=\"price price--hrk\">[^\w]*([^<]*)", page)
         date = re.findall("<time class=\"date[^>]*>([^<]*)</time>", page)
-        img = re.findall("<img class=\"img entity-thumbnail-img\" src=\"([^\"]*)\"", page)
-        #print(len(titles))
-        #print(len(desc))
-        #print(len(price))
-        #print(len(date))
-        #print(len(img))
-        """
+        img = re.findall("<img class=\"img entity-thumbnail-img.*\ssrc=\"([^\"]*)\"", page)
+        #print(price)
         for i in range(len(titles)):
-            print(titles[i])
             data.append(dict(
-                Title = titles[i],
+                Title=titles[i],
                 Description=desc[i],
-                Price=" ".join(price[i]),
+                Price=price[i].replace("&nbsp;", ""),
                 PublishedDate=date[i],
                 ImageUrl=img[i]
             ))
-        """
     return json.dumps(data)
 
 
@@ -85,28 +84,40 @@ def xpath(page, site):
 
     if site == "rtv":
         data = dict(
-            Author=tree.xpath('//*[@id="main-container"]//*[@class="article-meta"]//div[@class="author-name"]/text()')[0].strip(),
+            Author=tree.xpath('//*[@id="main-container"]//*[@class="article-meta"]//div[@class="author-name"]/text()')[
+                0].strip(),
             PublishedTime=tree.xpath('//*[@id="main-container"]//*[@class="publish-meta"]/text()')[0].strip(),
             Title=tree.xpath('//*[@id="main-container"]//header/h1/text()')[0].strip(),
             SubTitle=tree.xpath('//*[@id="main-container"]//header/div[@class="subtitle"]/text()')[0].strip(),
             Lead=tree.xpath('//*[@id="main-container"]//header/p[@class="lead"]/text()')[0].strip(),
-            Content="".join([p + "\n" for p in tree.xpath('//*[@id="main-container"]//div[@class="article-body"]//*[not(self::script)]/text()[normalize-space()]')])
+            Content="".join([p + "\n" for p in tree.xpath(
+                '//*[@id="main-container"]//div[@class="article-body"]//*[not(self::script)]/text()[normalize-space()]')])
         )
     elif site == "overstock":
         data = []
         i = 1
         retries = 0
         while retries < 3:
-            title = tree.xpath(f'//table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[@bgcolor][{i}]/td[2]/a//text()')
+            title = tree.xpath(
+                f'//table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[@bgcolor][{i}]/td[2]/a//text()')
             if title:
                 retries = 0
                 data.append(dict(
                     Title=title[0].strip(),
-                    ListPrice=tree.xpath(f'//table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[@bgcolor][{i}]/td[2]/table//table//tr[1]/td[2]//text()[normalize-space()]')[0].strip(),
-                    Price=tree.xpath(f'//table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[@bgcolor][{i}]/td[2]/table//table//tr[2]/td[2]//text()[normalize-space()]')[0].strip(),
-                    Saving=tree.xpath(f'//table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[@bgcolor][{i}]/td[2]/table//table//tr[3]/td[2]/span//text()')[0].split()[0].strip(),
-                    SavingPercent=tree.xpath(f'//table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[@bgcolor][{i}]/td[2]/table//table//tr[3]/td[2]/span//text()')[0].split()[1].strip(),
-                    Content="".join(tree.xpath(f'//table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[@bgcolor][{i}]/td[2]/table/tbody/tr/td[2]//text()[normalize-space()]'))
+                    ListPrice=tree.xpath(
+                        f'//table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[@bgcolor][{i}]/td[2]/table//table//tr[1]/td[2]//text()[normalize-space()]')[
+                        0].strip(),
+                    Price=tree.xpath(
+                        f'//table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[@bgcolor][{i}]/td[2]/table//table//tr[2]/td[2]//text()[normalize-space()]')[
+                        0].strip(),
+                    Saving=tree.xpath(
+                        f'//table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[@bgcolor][{i}]/td[2]/table//table//tr[3]/td[2]/span//text()')[
+                        0].split()[0].strip(),
+                    SavingPercent=tree.xpath(
+                        f'//table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[@bgcolor][{i}]/td[2]/table//table//tr[3]/td[2]/span//text()')[
+                        0].split()[1].strip(),
+                    Content="".join(tree.xpath(
+                        f'//table[2]/tbody/tr[1]/td[5]/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr[@bgcolor][{i}]/td[2]/table/tbody/tr/td[2]//text()[normalize-space()]'))
                 ))
             else:
                 retries += 1
@@ -117,10 +128,18 @@ def xpath(page, site):
         for i, title in enumerate(titles):
             data.append(dict(
                 Title=title.strip(),
-                Description=tree.xpath(f'//div[@class="content-main"]//ul/li/article//div[@class="entity-description-main"]//text()[normalize-space()]')[i].strip(),
-                Price=tree.xpath(f'//div[@class="content-main"]//ul/li/article//div[@class="entity-prices"]//strong[@class="price price--hrk"]/text()[normalize-space()]')[i].strip() + " €",
-                PublishedDate=tree.xpath(f'//div[@class="content-main"]//ul/li/article//div[@class="entity-pub-date"]/time/text()[normalize-space()]')[i].strip(),
-                ImageUrl=tree.xpath(f'//div[@class="content-main"]//ul/li/article/div[@class="entity-thumbnail"]/a/img/@src')[i].strip(),
+                Description=tree.xpath(
+                    f'//div[@class="content-main"]//ul/li/article//div[@class="entity-description-main"]//text()[normalize-space()]')[
+                    i].strip(),
+                Price=tree.xpath(
+                    f'//div[@class="content-main"]//ul/li/article//div[@class="entity-prices"]//strong[@class="price price--hrk"]/text()[normalize-space()]')[
+                          i].strip() + " €",
+                PublishedDate=tree.xpath(
+                    f'//div[@class="content-main"]//ul/li/article//div[@class="entity-pub-date"]/time/text()[normalize-space()]')[
+                    i].strip(),
+                ImageUrl=
+                tree.xpath(f'//div[@class="content-main"]//ul/li/article/div[@class="entity-thumbnail"]/a/img/@src')[
+                    i].strip(),
             ))
     return json.dumps(data)
 
